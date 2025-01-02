288eebc05cac67124dded5b14863a703eca48474
sazzad <sazzadador@gmail.com>
2025-01-03 00:02:17 +0600

    query by email

--- index.js
+++ index.js
@@ -33,9 +33,17 @@ async function run() {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
-    app.get("/carts", async (req, res) => {
+    
+
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
     // await client.connect();
