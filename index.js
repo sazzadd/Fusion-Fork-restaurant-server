7b88c4d9f7f7a18a44e31083ec9e06efdffdc8a9
sazzad <sazzadador@gmail.com>
2025-01-05 23:56:32 +0600

    users collection create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(expres.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krhx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +24,13 @@ async function run() {
   const menuCollection = client.db("fusionFork").collection("menu");
   const reviewsCollection = client.db("fusionFork").collection("reviews");
   const cartCollection = client.db("fusionFork").collection("carts");
+  const userCollection = client.db("fusionFork").collection("users");
   try {
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -33,7 +39,6 @@ async function run() {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
-    
 
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
@@ -42,8 +47,14 @@ async function run() {
     });
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      const query = {email: email}
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
     // await client.connect();
